f17149dbd7af906a9f73f8079d87d37ab6e86de0
hxcy1999 <whx903157527@163.com>
2022-09-15 23:20:22 -0500

    change reverse listnode

--- src/leetcode/ReverLinkedList206.java
+++ src/leetcode/ReverLinkedList206.java
@@ -3,17 +3,25 @@ package leetcode;
 public class ReverLinkedList206 {
     public static void main(String[] args) {
         ListNode head = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4))));
-        ListNode reverse = new ListNode();
-        ListNode next;
-
-        while (head != null ){
-            next = head.next;
-            head.next = reverse.next;
-            reverse.next = head;
-                head =next;
+//        ListNode reverse = new ListNode();
+//        ListNode next;
+//
+//        while (head != null ){
+//            next = head.next;
+//            head.next = reverse.next;
+//            reverse.next = head;
+//                head =next;
+//        }
+//        System.out.println(reverse);
+        ListNode curr = head;
+        ListNode pre = null;
+        while (curr != null){
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
         }
-        System.out.println(reverse);
-
+        System.out.print(pre);
     }
 
 }
